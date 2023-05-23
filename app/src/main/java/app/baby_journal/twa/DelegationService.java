a7e77c4d3f39a87a30db17c9f18ca7bec6b80413
Dominik Chrastecky <dominik@chrastecky.cz>
2023-05-23 02:07:39 +0200

    Initial commit

---
+++ app/src/main/java/app/baby_journal/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.baby_journal.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
